b088afbe83e3a43aa9328912fff1d7543547abda
KrishxVed-69 <krishsaini688@gmail.com>
2025-06-03 11:22:38 +0530

    commit: not applicable at once so commented out the LOCAL url for now

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-  baseURL: process.env.REACT_APP_API_URL
+  //baseURL: process.env.REACT_APP_API_BASE_URL, #Local development URL
+  baseURL: process.env.REACT_APP_API_URL //Production URL
 });
 
 export default api;
